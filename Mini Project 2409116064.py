4c6c1159d127b5c50949906acceea78126ddd4d3
darelprasetya <darel2227@gmail.com>
2024-09-30 21:22:40 +0800

    Add files via upload

---
+++ Mini Project 2409116064.py
@@ -0,0 +1,48 @@
+# inputan login
+print("-----LOGIN-----")
+nama =  input("Masukkan Nama: ")
+nim =   input("Masukkan NIM: ")
+kelas = input("Masukkan Kelas: ")
+
+print("\nSelamat anda telah berhasil login,", nama)
+print("NIM: ", nim)
+print("Kelas: ", kelas)
+
+# membuat daftar barang
+barang = {
+    "Baju":150000,
+    "Celana":250000,
+    "Tas":200000,
+}.lower()
+print("----------------------DAFTAR BARANG-----------------------")
+for a in barang:
+    print("Daftar Barang:", a,"\t Harga :", barang[a])
+print("--------------------------------------------------------")
+
+#menginput barang
+while True:
+            pilih_barang = input("Barang yang dipesan: ",)
+            jumlah_pembelian = int(input("Jumlah Pesanan: "))
+            
+            total_harga = jumlah_pembelian * barang[pilih_barang]
+
+# menentukan diskon
+            if total_harga > 250000:
+                diskon = total_harga*25/100
+                total = total_harga - diskon
+                print("\nSelamat anda mendapatkan diskon sebesar 25%")
+            else:
+                total = total_harga 
+
+            print("----------------Pembayaran----------------")
+            print("Barang yang dipesan      : ", pilih_barang)
+            print("Jumlah yang dipesan      : ", jumlah_pembelian)
+            print("Total biaya              : ", total_harga)
+            print("Total yang harus dibayar : ", total)
+            print("------------------------------------------")
+
+        # mengulang
+            ulang = input("Apakah ingin menghitung ulang? (yes/no) : ").lower()
+            if ulang != 'yes':
+                print("Terima kasih telah berbelanja disini")
+                break
